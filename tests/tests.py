638e856834ac08768e4a0c2389789a2a3d7d6b59
Vugar <vugar254862@gmail.com>
2024-03-10 14:17:21 +0200

    i added print

--- tests/tests.py
+++ tests/tests.py
@@ -5,6 +5,31 @@ from selenium.webdriver.common.by import By
 from common.actions import Actions
 from test_data import test_data as TD
 def test_2(driver):
+    action = Actions(driver)
     driver.get(TD.base_url)
     time.sleep(6)
+    action.send_keys('hi')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print
