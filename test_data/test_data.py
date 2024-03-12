f11afc00331f8093a5caace8aeebebddf693e969
User <ozmiz2001@gmail.com>
2024-03-12 13:10:40 +0200

    file new

--- test_data/test_data.py
+++ test_data/test_data.py
@@ -20,3 +20,32 @@ cancel_purchase_btn = 'aaaa'
 approve_cancel_purchase_btn = "aa"
 approve_continue_purchase_btn = "aa"
 continue_purchase_btn = "ff"
+
+categories = (By.XPATH,"//body/div[@id='root']/main[1]/div[2]/div[2]/div[1]/div[1]/div[2]/a[5]")
+location = (By.XPATH,"//div[@id='rc-tabs-0-tab-tab4']")
+
+purple_btn = (By.XPATH,"//*[contains(@class, 'px-8 py-2 bg-primaryPurple text-white text)]")
+
+first_cat = (By.XPATH,'//body/div[@id="root"]/div[2]/div[2]/div[1]/div[1]/div[2]/a[3]')
+event_tickets = "//*[contains(@class, 'hidden md:grid grid-cols-1 md:grid-cols-5 text-[18px] my-4')]"
+
+logo =  (By.XPATH,"//*[@class='hidden md:flex space-x-10 md:w-[500px]']")
+picture = (By.XPATH,'//body/div[@id="root"]/main[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[4]/a[1]/div[1]/img[1]')
+# all_locations = driver.find_elements(By.XPATH,'//*[@class="py-4"]')
+# for event in all_locations:
+#     event.click()
+#     event_tickets = driver.find_elements(By.XPATH,TD.event_ticket)
+#     for ticket in event_tickets:
+#         purple_btn = actions.find_element(TD.purple_btn)
+#         innertext = purple_btn.get_attribute('innerText')
+#         if innertext == "לרכישה":
+#             purple_btn.click()
+#             time.sleep(10)
+#             break
+#         else:
+#             logo = actions.find_element(TD.logo)
+#             logo.click()
+#             time.sleep(10)
+#
+#
+#
--- tests/tests.py
+++ tests/tests.py
@@ -3,75 +3,73 @@ import pytest
 from selenium.webdriver.common.by import By
 from common.actions import Actions
 from test_data import test_data as TD
-
-def test_login(driver):
-    actions = Actions(driver)
-    driver.get(TD.base_url)
-
-    account = actions.find_element(TD.login_btn)
-    account.click()
-
-    phone = actions.find_element(TD.phone_number_input)
-    phone.click()
-    phone.send_keys(TD.oz_phone)
-
-    enter = actions.find_element(TD.enter_btn)
-    enter.click()
-
-    code_box = actions.find_element(TD.enter_code_box)
-    actions.input_text(code_box)
-    time.sleep(15)
-
-    something_btn = actions.find_element(TD.enter_code_btn)
-    something_btn.click()
-
-    assert driver.current_url == TD.base_url
-
-
-def test_timer_func(driver):
-    actions = Actions(driver)
-    driver.get(TD.base_url)
-
-    event_ticket = actions.find_element(TD.event_ticket)
-    event_ticket.click()
-
-    buy_btn = actions.find_element(TD.buy_btn)
-    buy_btn.click()
-
-    pop_up_otp = actions.find_element(TD.pop_up_otp)
-    actions.input_text(pop_up_otp)
-
-    pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
-    pop_up_otp_accept_btn.click()
-
-    assert 'something' == 'something'
-
-
-def test_timer_expire_cancel(driver):
-    actions = Actions(driver)
-    driver.get(TD.base_url)
-
-    event_ticket = actions.find_element(TD.event_ticket)
-    event_ticket.click()
-
-    buy_btn = actions.find_element(TD.buy_btn)
-    buy_btn.click()
-
-    pop_up_otp = actions.find_element(TD.pop_up_otp)
-    actions.input_text(pop_up_otp)
-
-    pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
-    pop_up_otp_accept_btn.click()
-
-    cancel_purchase_btn = actions.find_element(TD.cancel_purchase_btn)
-    cancel_purchase_btn.click()
-
-    approve_cancel_purchase_btn = actions.find_element(TD.approve_cancel_purchase_btn)
-    approve_cancel_purchase_btn.click()
-
-    assert driver.current_url == TD.base_url
-
-
+# @pytest.mark.skip
+# def test_login(driver):
+#     actions = Actions(driver)
+#     driver.get(TD.base_url)
+#
+#     account = actions.find_element(TD.login_btn)
+#     account.click()
+#
+#     phone = actions.find_element(TD.phone_number_input)
+#     phone.click()
+#     phone.send_keys(TD.oz_phone)
+#
+#     enter = actions.find_element(TD.enter_btn)
+#     enter.click()
+#
+#     time.sleep(15)
+#
+#     something_btn = actions.find_element(TD.enter_code_btn)
+#     something_btn.click()
+#
+#     assert driver.current_url == TD.base_url
+#
+# @pytest.mark.skip
+# def test_timer_func(driver):
+#     actions = Actions(driver)
+#     driver.get(TD.base_url)
+#
+#     event_ticket = actions.find_element(TD.event_ticket)
+#     event_ticket.click()
+#
+#     buy_btn = actions.find_element(TD.buy_btn)
+#     buy_btn.click()
+#
+#     pop_up_otp = actions.find_element(TD.pop_up_otp)
+#     actions.input_text(pop_up_otp)
+#
+#     pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
+#     pop_up_otp_accept_btn.click()
+#
+#     assert 'something' == 'something'
+#
+# @pytest.mark.skip
+# def test_timer_expire_cancel(driver):
+#     actions = Actions(driver)
+#     driver.get(TD.base_url)
+#
+#     event_ticket = actions.find_element(TD.event_ticket)
+#     event_ticket.click()
+#
+#     buy_btn = actions.find_element(TD.buy_btn)
+#     buy_btn.click()
+#
+#     pop_up_otp = actions.find_element(TD.pop_up_otp)
+#     actions.input_text(pop_up_otp)
+#
+#     pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
+#     pop_up_otp_accept_btn.click()
+#
+#     cancel_purchase_btn = actions.find_element(TD.cancel_purchase_btn)
+#     cancel_purchase_btn.click()
+#
+#     approve_cancel_purchase_btn = actions.find_element(TD.approve_cancel_purchase_btn)
+#     approve_cancel_purchase_btn.click()
+#
+#     assert driver.current_url == TD.base_url
+#
+@pytest.mark.skip
 def test_timer_expire_continue(driver):
     actions = Actions(driver)
     driver.get(TD.base_url)
@@ -95,32 +93,72 @@ def test_timer_expire_continue(driver):
     approve_continue_purchase_btn.click()
 
     assert driver.current_url == driver.current_url
+#
+#
+# @pytest.mark.skip
+# # @pytest.mark.parametrize('payment_option_path, inner_text',
+# #     ['//*[@a "google"]','google'],
+# #     ['//*[@a "paypal"]','paypal'],
+# #     ['//*[@a "apple pay"]','apple pay'],
+# #     ['//*[@a "credit"]','credit'])
+# def test_payment_options(driver,payment_option_path,inner_text):
+#     actions = Actions(driver)
+#     driver.get(TD.base_url)
+#
+#     event_ticket = actions.find_element(TD.event_ticket)
+#     event_ticket.click()
+#
+#     buy_btn = actions.find_element(TD.buy_btn)
+#     buy_btn.click()
+#
+#     pop_up_otp = actions.find_element(TD.pop_up_otp)
+#     actions.input_text(pop_up_otp)
+#
+#     pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
+#     pop_up_otp_accept_btn.click()
+#
+#     payment_option = actions.find_element(payment_option_path)
+#     payment_option_text = payment_option.get_attribute('innerText')
+#     assert payment_option_text == inner_text
 
-
-
-@pytest.mark.parametrize('payment_option_path, inner_text',
-    ['//*[@a "google"]','google'],
-    ['//*[@a "paypal"]','paypal'],
-    ['//*[@a "apple pay"]','apple pay'],
-    ['//*[@a "credit"]','credit'])
-def test_payment_options(driver,payment_option_path,inner_text):
+def test_for_loop(driver):
     actions = Actions(driver)
     driver.get(TD.base_url)
 
-    event_ticket = actions.find_element(TD.event_ticket)
-    event_ticket.click()
+    account = actions.find_element(TD.login_btn)
+    account.click()
 
-    buy_btn = actions.find_element(TD.buy_btn)
-    buy_btn.click()
+    phone = actions.find_element(TD.phone_number_input)
+    phone.click()
+    phone.send_keys(TD.oz_phone)
 
-    pop_up_otp = actions.find_element(TD.pop_up_otp)
-    actions.input_text(pop_up_otp)
+    enter = actions.find_element(TD.enter_btn)
+    enter.click()
 
-    pop_up_otp_accept_btn = actions.find_element(TD.pop_up_otp_accept_btn)
-    pop_up_otp_accept_btn.click()
+    time.sleep(15)
 
-    payment_option = actions.find_element(payment_option_path)
-    payment_option_text = payment_option.get_attribute('innerText')
-    assert payment_option_text == inner_text
+    something_btn = actions.find_element(TD.enter_code_btn)
+    something_btn.click()
+
+    sport_options = actions.find_element(TD.first_cat)
+    sport_options.click()
+
+    location = actions.find_element(TD.location)
+    location.click()
+
+    event = actions.find_element(TD.picture)
+    event.click()
+
+    event_tickets = driver.find_elements(By.XPATH,"//*[@xpath='1']")
+    for i in event_tickets:
+        purple_btn = actions.find_element(TD.purple_btn)
+        innertext = purple_btn.get_attribute('innerText')
+        if innertext in i == "לרכישה":
+            purple_btn.click()
+            time.sleep(10)
+            break
+
+
+
 
 
