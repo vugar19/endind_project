40deec65831ca8748177c8bfdbd6c558e93087e2
noamt <noamtpyster@gmail.com>
2024-03-13 21:18:38 +0200

    changed hello to goodbye

---
+++ tests/draft_noam.py
@@ -0,0 +1,19 @@
+
+next_botton = '//*[@id="root"]/main/div[2]/div[2]/div/div/div[3]/button'
+verify_ticket = '//*[@id="root"]/main/div[2]/div[2]/div/div/div[4]/div[2]/form/button'
+card_number_field = '//*[@id="cardNumber"]'
+card_number_input = '4032038134874225'
+month_august = '//*[@id="expmonth"]/option[9]'
+year_2026 = '//*[@id="expyear"]/option[4]'
+security_code_field = '//*[@id="payment_section_2"]/div[3]/div[1]'
+security_code_input = '265'
+pay = '//*[@id="submitbtn"]'
+
+
+
+click next_botton
+enter verification code
+click verify_ticket
+in card_number_field enter card_number
+click month_august
+click year_2026
